e152b1a74f9e46536a8dabf9d85d8b4ef0cec3b2
John Muchovej <ionlights@users.noreply.github.com>
2018-08-29 15:11:17 -0400

    idk ssh port forwarding. lol.

--- fa18/launch.sh
+++ fa18/launch.sh
@@ -10,5 +10,5 @@ echo "JobID: $batch"
 host="$(squeue -j $batch -o %N | grep evc)"
 
 echo -e "Now that it's running, you need to run (in a new terminal window):\n"
-echo -e "    ssh -NL $PORT:$host:19972 $USER@newton.ist.ucf.edu"
+echo -e "    ssh -NL 19972:$host:$PORT $USER@newton.ist.ucf.edu"
 echo -e "\nOnce you've run ^, open a browser and head to http://localhost:19972/"
