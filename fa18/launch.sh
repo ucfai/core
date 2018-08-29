4faa324be6f001a8eb51d52a1c475412acc17f98
John Muchovej <jmuchovej@users.noreply.github.com>
2018-08-29 14:36:05 -0400

    testing if hostname is passable

--- fa18/launch.sh
+++ fa18/launch.sh
@@ -9,5 +9,5 @@ echo "$MY_HOST"
 
 echo "\nSpinning up your Jupyter Notebook!"
 echo "\nNow that it's running, you need to run:"
-echo "\n    ssh -NL $PORT:$(hostname):19972 $USER@newton.ist.ucf.edu"
+echo "\n    ssh -NL $PORT:$MY_HOST:19972 $USER@newton.ist.ucf.edu"
 echo "Once you've run ^, open a browser and head to http://localhost:19972/"
