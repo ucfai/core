331a230c9d2f3a99a3eedaaf23397f554c4c6e54
John Muchovej <jmuchovej@users.noreply.github.com>
2018-08-29 14:27:02 -0400

    attempting to resolve hostname

--- fa18/launch.sh
+++ fa18/launch.sh
@@ -5,7 +5,9 @@ PORT="$UID"
 batch="$(sbatch --parsable jupyter-notebooks.slurm $PORT)"
 echo -e "$batch"
 
+echo -e "$MY_HOST"
+
 echo -e "\nSpinning up your Jupyter Notebook!"
 echo -e "\nNow that it's running, you need to run:"
 echo -e "\n    ssh -NL $PORT:$(hostname):19972 $USER@newton.ist.ucf.edu"
-echo -e "\nOnce you've run ^, open a browser and head to http://localhost:19972/"
+echo -e "Once you've run ^, open a browser and head to http://localhost:19972/"
