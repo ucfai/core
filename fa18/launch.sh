4eed09b3e8c4358d7bf75b467eeec55b6b9e02f2
John Muchovej <ionlights@users.noreply.github.com>
2018-08-29 15:17:18 -0400

    altering messages and shifting styling around

--- fa18/launch.sh
+++ fa18/launch.sh
@@ -5,10 +5,10 @@ PORT="$UID"
 echo -e "Spinning up your Jupyter Notebook! (Give me about 5s...)"
 batch="$(sbatch --parsable jupyter-notebooks.slurm $PORT)"
 sleep 5
-echo "\nJobID: $batch"
+echo -e "\nJobID: $batch"
 
 host="$(squeue -j $batch -o %N | grep evc)"
 
-echo -e "\nNow that it's running, you need to run (in a new terminal window):"
+echo -e "\nNow that it's running, you need to run (open a new terminal and paste this):"
 echo -e "\n    ssh -NL 19972:$host:$PORT $USER@newton.ist.ucf.edu"
-echo -e "\nOnce you've run ^, open a browser and head to http://localhost:19972/"
+echo -e "\nOnce you've run the above command, open a browser and head to http://localhost:19972/"
