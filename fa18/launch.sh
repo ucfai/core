9f1227af785e9e5191873fbfcff6ba078f93a027
John Muchovej <ionlights@users.noreply.github.com>
2018-08-29 15:19:43 -0400

    enforcing use of ssh-config given out in lectures

--- fa18/launch.sh
+++ fa18/launch.sh
@@ -10,5 +10,5 @@ echo -e "\nJobID: $batch"
 host="$(squeue -j $batch -o %N | grep evc)"
 
 echo -e "\nNow that it's running, you need to run (open a new terminal and paste this):"
-echo -e "\n    ssh -NL 19972:$host:$PORT $USER@newton.ist.ucf.edu"
+echo -e "\n    ssh -NL 19972:$host:$PORT sigai.newton"
 echo -e "\nOnce you've run the above command, open a browser and head to http://localhost:19972/"
