f1b0626913e5a337cb6d24c2cd362c1af68c0e34
John Muchovej <ionlights@users.noreply.github.com>
2018-08-29 14:18:49 -0400

    attempting to add batch filtering

--- fa18/launch.sh
+++ fa18/launch.sh
@@ -3,8 +3,9 @@
 PORT="$UID"
 
 batch="$(sbatch --parsable jupyter-notebooks.slurm $PORT)"
+echo -e "$batch"
 
 echo -e "\nSpinning up your Jupyter Notebook!"
 echo -e "\nNow that it's running, you need to run:"
-echo -e "\n    ssh -NL $PORT:$(hostname):19972 ${USER}@newton.ist.ucf.edu"
+echo -e "\n    ssh -NL $PORT:$(hostname):19972 $USER@newton.ist.ucf.edu"
 echo -e "\nOnce you've run ^, open a browser and head to http://localhost:19972/"
